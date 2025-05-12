1d21d17676a7b2193de5ac1cde06f1cbdfa3e753
yuricsg <yuriicardo004@gmail.com>
2025-05-12 19:23:18 -0300

    first commit

---
+++ src/main/java/com/cesar/school/sensetrack/yuricsg/SenseTrackApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.school.sensetrack.yuricsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/school/sensetrack/yuricsg/SenseTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.school.sensetrack.yuricsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
